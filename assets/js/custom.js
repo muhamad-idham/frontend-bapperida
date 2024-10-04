c3696a0f3a0047cbc0a43acf14840a12977a1c8f
Muhamad Idham <idhammuhammad23@gmail.com>
2024-10-04 14:42:08 +0700

    new project: fe-bapperida v2.0

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $('#owl-slide').owlCarousel({
+//         navigation : false, // Show next and prev buttons
+//         slideSpeed : 300,
+//         paginationSpeed : 400,
+//         items: 1,
+//         dots: false,
+//         rtl: true,
+//         autoplay:true,
+//         autoplayTimeout:10000,
+//     })
+// });
